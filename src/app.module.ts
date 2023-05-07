2ea1931d68b14ceb1ee3d149b4d168f70f6037a2
Ehtesham Shahzad <ehtesham@we-over-i.com>
2023-05-07 19:59:08 +0500

    Setting up graphql

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      /*
+      * Using join so instead of saving scheme in memory we are saving it in a director to make sure graphQL types look as expected
+      */
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
